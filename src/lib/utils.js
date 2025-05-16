ef94bb3bb030ce126c2ecd8617fab9dd17c59d75
czwaiwai <czwaiwai@msn.cn>
2025-05-16 11:01:29 +0800

    去掉手机适配

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -461,5 +461,6 @@ export function filterSmall(arr, indexs) {
 }
 
 export function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  return false
+  // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 }
